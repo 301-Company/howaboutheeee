96ba43c12224f8804fec8b6dc34dc15a1a1e1eb3
wjinss <aastu4343@gmail.com>
2025-04-24 20:39:52 +0900

    nav컴포넌트 클래스 명 추가 및 스타일링 작업

--- src/app/components/nav.tsx
+++ src/app/components/nav.tsx
@@ -1,4 +1,7 @@
+import Image from "next/image";
 import Link from "next/link";
+import styles from "../styles/nav.module.css";
+import searchIcon from "../../../public/icon_search.svg";
 
 const links = [
   { name: "어워드", href: "/award" },
@@ -8,14 +11,16 @@ const links = [
 
 export default function Nav() {
   return (
-    <header>
+    <header className={styles.header}>
       <h1>
         <Link href="/">로고</Link>
       </h1>
-      <nav>
+      <nav className={styles.nav}>
         <ul>
           <li>
-            <div>검색</div>
+            <button>
+              <Image src={searchIcon} alt="검색" width={20} height={20} />
+            </button>
           </li>
           {links.map((link) => (
             <li key={link.name}>
